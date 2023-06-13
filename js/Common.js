cf05ff39ea5bc6ce8c3962445dd8d23cc595fd2a
EUNSOLY <dmsthf9596@gmail.com>
2023-06-13 15:51:46 +0900

    23.06.13 포트폴리오제작

--- js/Common.js
+++ js/Common.js
@@ -10,3 +10,79 @@ function slideTextAnimation() {
 }
 
 setInterval(slideTextAnimation, 2500);
+
+// 스킬
+const $tapBox = document.querySelector(".tapBox");
+const $skillIcon = document.querySelectorAll(".skillList > li");
+let listData;
+fetch("./data/skillTextData.json")
+  .then((response) => response.json())
+  .then((data) => {
+    listData = data;
+    // JSON 데이터를 사용하는 로직 작성
+    listData.forEach((item, i) => {
+      let HTML = ``;
+      if (Array.isArray(item.title)) {
+        // "Graphics" 및 "Cooperation" 객체의 경우
+        // console.log(item, "이미지");
+        const graphicsSkills1 = item.skills
+          .slice(0, 2) // 배열의 0번째부터 1번째까지만 추출
+          .map((skill) => {
+            const imgSrc = `./image/SKILL/${skill}.svg`;
+            return `<li><img src="${imgSrc}" alt="${skill}" /></li>`;
+          })
+          .join("");
+
+        const graphicsSkills2 = item.skills
+          .slice(2, 5) // 배열의 2번째부터 4번째까지만 추출
+          .map((skill) => {
+            const imgSrc = `./image/SKILL/${skill}.svg`;
+            return `<li><img src="${imgSrc}" alt="${skill}" /></li>`;
+          })
+          .join("");
+        // console.log(graphicsSkills1, "확인값1");
+        // console.log(graphicsSkills2, "확인값2");
+        HTML += `
+          <div class="skillTextCon">
+          <h3>${item.title[0]}</h3>
+          <ul class="skillText skillImgCon">
+          ${graphicsSkills1}
+          </ul> 
+          <h3>${item.title[1]}</h3>
+          <ul class="skillText skillImgCon">
+          ${graphicsSkills2}
+          </ul>
+          </div>
+          `;
+      } else {
+        const skillItems = item.skills
+          .map((skill) => `<li>${skill}</li>`)
+          .join("");
+
+        HTML += `
+          <div class="skillTextCon">
+          <h3>${item.title}</h3>
+          <ul class="skillText">
+          ${skillItems}
+          </ul>
+          </div>
+          `;
+      }
+
+      $tapBox.innerHTML += HTML;
+      const $skillText = $tapBox.querySelectorAll("div");
+      $skillText[0].classList.add("on");
+      $skillIcon.forEach((iconitem, index) => {
+        iconitem.addEventListener("click", () => {
+          $skillText.forEach((item, textIndex) => {
+            item.classList.remove("on");
+          });
+          $skillText[index].classList.add("on");
+        });
+      });
+    });
+  })
+  .catch((error) => {
+    // 오류 처리
+    console.log("Error:", error);
+  });
