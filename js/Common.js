df5c52e8af7b234d0a8cbbfeabaf7a8d9f07dbd0
EUNSOLY <dmsthf9596@gmail.com>
2023-06-14 14:15:06 +0900

    23.06.14 슬라이드오류해결

--- js/Common.js
+++ js/Common.js
@@ -14,6 +14,79 @@ function showMenu(menuId) {
     const selectedMenu = document.querySelector("main.Home");
     selectedMenu.classList.add("on");
   }
+  if (menuId == "Project") {
+    // 프로젝트 슬라이드 구현
+
+    const $slideCon = document.querySelector(".contentCon ul");
+    const $slides = document.querySelectorAll(".contentCon li");
+    const $dotNav = document.querySelector(".dotnavi");
+
+    let currentSlide = 0; // 증가값 초기화
+    let sliderCount = $slides.length; // 슬라이드 갯수
+    let slideWidth = $slides[0].offsetWidth; // 슬라이드 1개의 크기
+    let dotIndex = ""; // 도트 초기화
+    let intervalId; // interval초기화셋팅
+
+    // 화면크기에 따른 슬라이드 반응형
+    window.addEventListener("resize", () => {
+      slideWidth = $slides[0].offsetWidth;
+    });
+
+    // 도트 생성
+    $slides.forEach((item, i) => {
+      i === currentSlide ? (active = "on") : (active = "");
+      dotIndex += `<span class='dot ${active}' data-index="${i}"></span>`;
+      $dotNav.innerHTML = dotIndex;
+    });
+
+    // 도트 실행
+    function dotActive() {
+      let $dot = $dotNav.querySelectorAll("span");
+      $dot.forEach((item, i) => {
+        item.classList.remove("on");
+        $dot[currentSlide].classList.add("on");
+      });
+    }
+
+    // 도트클릭이벤트
+    let $dot = $dotNav.querySelectorAll("span");
+    $dot.forEach((dot, i) => {
+      dot.addEventListener("click", () => {
+        const clickedIndex = dot.getAttribute("data-index");
+        currentSlide = parseInt(clickedIndex) - 1;
+        clearInterval(intervalId);
+        moveSlide();
+        intervalId = setInterval(moveSlide, 3000);
+      });
+    });
+
+    //슬라이드구현
+    $slideCon.appendChild($slides[0].cloneNode(true)); // 첫 번째 슬라이드를 복사하여 뒤에 추가
+
+    // 슬라이드 콜백함수
+    function moveSlide() {
+      $slideCon.style.transition = "0.5s";
+      $slideCon.style.marginLeft = `-${slideWidth * (currentSlide + 1)}px`;
+
+      console.log(currentSlide, "++전");
+
+      currentSlide++;
+
+      console.log(`-${slideWidth * (currentSlide + 1)}px`);
+      if (currentSlide === sliderCount) {
+        // 마지막 슬라이드에 도달한 경우 첫 번째 슬라이드로 되돌아감
+        setTimeout(() => {
+          $slideCon.style.transition = "none";
+          $slideCon.style.marginLeft = "0";
+          // 첫 번째 슬라이드 복사본 삭제
+        }, 1000);
+        currentSlide = 0;
+      }
+      dotActive(currentSlide);
+    }
+
+    intervalId = setInterval(moveSlide, 3000); // 자동슬라이드 실행
+  }
 }
 
 headLink.forEach((link, i) => {
@@ -126,75 +199,3 @@ fetch("./data/skillTextData.json")
     // 오류 처리
     console.log("Error:", error);
   });
-
-// 프로젝트 슬라이드 구현
-//
-const $slideCon = document.querySelector(".contentCon ul");
-const $slides = document.querySelectorAll(".contentCon li");
-const $dotNav = document.querySelector(".dotnavi");
-
-let currentSlide = 0; // 증가값 초기화
-let sliderCount = $slides.length; // 슬라이드 갯수
-let slideWidth = $slides[0].offsetWidth; // 슬라이드 1개의 크기
-let dotIndex = ""; // 도트 초기화
-let intervalId; // interval초기화셋팅
-
-// 화면크기에 따른 슬라이드 반응형
-window.addEventListener("resize", () => {
-  slideWidth = $slides[0].offsetWidth;
-});
-
-// 도트 생성
-$slides.forEach((item, i) => {
-  i === currentSlide ? (active = "on") : (active = "");
-  dotIndex += `<span class='dot ${active}' data-index="${i}"></span>`;
-  $dotNav.innerHTML = dotIndex;
-});
-
-// 도트 실행
-function dotActive() {
-  let $dot = $dotNav.querySelectorAll("span");
-  $dot.forEach((item, i) => {
-    item.classList.remove("on");
-    $dot[currentSlide].classList.add("on");
-  });
-}
-
-// 도트클릭이벤트
-let $dot = $dotNav.querySelectorAll("span");
-$dot.forEach((dot, i) => {
-  dot.addEventListener("click", () => {
-    const clickedIndex = dot.getAttribute("data-index");
-    currentSlide = parseInt(clickedIndex) - 1;
-    clearInterval(intervalId);
-    moveSlide();
-    intervalId = setInterval(moveSlide, 3000);
-  });
-});
-
-//슬라이드구현
-$slideCon.appendChild($slides[0].cloneNode(true)); // 첫 번째 슬라이드를 복사하여 뒤에 추가
-
-// 슬라이드 콜백함수
-function moveSlide() {
-  $slideCon.style.transition = "0.5s";
-  $slideCon.style.marginLeft = `-${slideWidth * (currentSlide + 1)}px`;
-
-  console.log(currentSlide, "++전");
-
-  currentSlide++;
-
-  console.log(`-${slideWidth * (currentSlide + 1)}px`);
-  if (currentSlide === sliderCount) {
-    // 마지막 슬라이드에 도달한 경우 첫 번째 슬라이드로 되돌아감
-    setTimeout(() => {
-      $slideCon.style.transition = "none";
-      $slideCon.style.marginLeft = "0";
-      // 첫 번째 슬라이드 복사본 삭제
-    }, 1000);
-    currentSlide = 0;
-  }
-  dotActive(currentSlide);
-}
-
-intervalId = setInterval(moveSlide, 3000); // 자동슬라이드 실행
