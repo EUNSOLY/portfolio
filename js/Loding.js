2b28a47fcc60d4828e9f1ea51b5c7ae956ab2626
EUNSOLY <dmsthf9596@gmail.com>
2023-06-14 16:15:17 +0900

    23.06.14 포트폴리오 제작_canvas태그

--- js/Loding.js
+++ js/Loding.js
@@ -1,16 +1,54 @@
-// const canvas = document.querySelector(".luckyDrowTool");
-// const ctx = canvas.getContext("2d");
-// // 캔버스의 너비와 높이 계산
-// const canvasWidth = canvas.width;
-// const canvasHeight = canvas.height;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+// 캔버스 크기 설정
+canvas.width = 1200; // 적절한 너비 값으로 변경해 주세요.
+canvas.height = 800; // 적절한 높이 값으로 변경해 주세요.
 
-// // 텍스트 스타일 설정
-// ctx.font = "7vw PilseungGothic";
-// ctx.textAlign = "center";
-// ctx.textBaseline = "middle";
+const image = new Image();
+image.src = "../image/TicketSvg.svg";
 
-// // 텍스트 그리기
-// const textX = canvasWidth / 2;
-// const textY = canvasHeight / 2;
-// ctx.fillText("Portfolio", textX, textY - 20);
-// ctx.fillText("Drow", textX, textY + 20);
+image.onload = function () {
+  // 이미지 비율 계산
+  const imageRatio = image.width / image.height;
+
+  // 캔버스에 맞춰서 이미지를 그리기 위한 가로와 세로 크기 계산
+  let drawWidth = canvas.width;
+  let drawHeight = drawWidth / imageRatio;
+  console.log(drawWidth);
+  if (drawHeight > canvas.height) {
+    drawHeight = canvas.height;
+    drawWidth = drawHeight * imageRatio;
+  }
+
+  // 이미지를 중앙에 그리기 위한 좌표 계산
+  const drawX = (canvas.width - drawWidth) / 2;
+  const drawY = (canvas.height - drawHeight) / 2;
+
+  // 배경색 설정
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // 텍스트 설정
+  const text = "Portfolio \n Drow";
+  const fontSize = 8;
+  const fontFamily = "PilseungGothic";
+
+  ctx.font = `${fontSize}vw ${fontFamily}`;
+  ctx.fillStyle = "black";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+
+  const lines = text.split("\n"); // 줄바꿈을 기준으로 텍스트를 나눕니다.
+  const lineHeight = fontSize * 18; // 줄 간격을 설정합니다.
+
+  lines.forEach((line, index) => {
+    const y =
+      canvas.height / 2 -
+      ((lines.length - 1) * lineHeight) / 2 +
+      index * lineHeight;
+    ctx.fillText(line, canvas.width / 2, y);
+  });
+
+  // 이미지를 캔버스에 그리기
+  ctx.drawImage(image, drawX, drawY, drawWidth, drawHeight);
+};
